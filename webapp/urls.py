9827e82671b1f06b0d5de5da2c6ff83652a87b0e
Georgi2313 <georgikjoseph@gmail.com>
2019-01-31 21:30:28 +0530

    added: base.html, code_details.html

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('x/<int:pk>/', views.code_detail, name='code_detail'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Pastebin
 
 def home(request):
-    return render(request,'webapp/home.html',{})
+    pastebin=Pastebin.objects.order_by('created_date')
+    return render(request,'webapp/home.html',{'pastebin':pastebin})
+
+def code_detail(request,pk):
+    x=get_object_or_404(Pastebin,pk=pk)
+    return render(request,'webapp/code_detail.html',{'x':x})
